d76f835b39875ec0139dadf46e746279b00bf38c
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-12-03 00:07:36 +0530

    code added

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
-const getFloors = document.querySelector(".number_of-floors");
-const getLifts = document.querySelector(".number_of-lifts");
+const floors = document.getElementsByClassName("number_of-floors");
+const lifts = document.getElementsByClassName("number_of-lifts");
+const buildButton = document.getElementById("build-btn");
+
+buildButton.addEventListener("click", function (){
+    alert("Lifts are busy please wait!");
+    });
