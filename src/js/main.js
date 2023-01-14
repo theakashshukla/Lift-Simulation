85982101448bdb382c7c9b9650a1ef1fb156526c
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-01-14 20:45:25 +0530

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,50 @@
-const floors = document.getElementsByClassName("number_of-floors");
-const lifts = document.getElementsByClassName("number_of-lifts");
-const buildButton = document.getElementById("build-btn");
+var buildButton = document.querySelector("#myButton");// get the button element
+var buildButton = document.querySelector("#myButton");
+var buildButton = document.querySelector("#build-btn");
 
-buildButton.addEventListener("click", function (){
-    alert("Lifts are busy please wait!");
-    });
+buildButton.addEventListener("click", function () {
+    buildLifts();
+    buildfloors();
+});
+
+function buildfloors() {
+
+}
+
+// function to generate multiple buttons with input text
+function buildLifts() {
+    // get input value
+    // var inputValue = document.querySelector("#build-btn").value;
+
+    // get number of buttons to generate
+    var numberOfButtons = document.querySelector("#num_of-fl").value;
+
+    // loop to create and append buttons
+    for (var i = 0; i < numberOfButtons; i++) {
+        // create button element
+        var button = document.createElement("button");
+        // set button text
+        // button.innerHTML = inputValue;
+        // append button to container element
+        document.querySelector("#container").appendChild(button);
+    }
+}
+
+// // function to generate multiple buttons with input text
+// function generateButtons() {
+//     // get input value
+//     var inputValue = document.querySelector("#input").value;
+
+//     // get number of buttons to generate
+//     var numberOfButtons = document.querySelector("#numberOfButtons").value;
+
+//     // loop to create and append buttons
+//     for (var i = 0; i < numberOfButtons; i++) {
+//         // create button element
+//         var button = document.createElement("button");
+//         // set button text
+//         button.innerHTML = inputValue;
+//         // append button to container element
+//         document.querySelector("#container").appendChild(button);
+//     }
+// }
